fd2c8c77f89994be233cc2e0372f14642672aefe
Shiva pandey <shivapandey9898@gmail.com>
2024-07-17 17:05:23 +0530

    feat: added heading one atom

---
+++ src/atoms/atoms-shiva/headings/one/six-xl/white/one-six-xl.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+const HeadingOneWhite = ({children}) => {
+    return(
+        <h1 className="text-4xl lg:text-6xl text-white font-geomanist_book">
+            { children }
+        </h1>
+    )
+}
+export default HeadingOneWhite
