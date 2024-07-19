6eed2a59b81d0c5d7e22a2c8d270f2861a020c74
Shiva pandey <shivapandey9898@gmail.com>
2024-07-19 11:07:05 +0530

    refactor: renamed import of atoms

--- src/components/home/hero/hero.jsx
+++ src/components/home/hero/hero.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
+
+// IMPORT REACT ICONS
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 
 // IMPORT REACT ATOMS
-import ButtonCyan from "@/atoms/atoms-shiva/button/cyan/large/button-cyan"
-import ButtonDark from "@/atoms/atoms-shiva/button/dark/button-dark"
-import ButtonOrange from "@/atoms/atoms-shiva/button/orange/button-orange"
-import HeadingOneWhite from "@/atoms/atoms-shiva/headings/one/six-xl/white/one-six-xl"
-import HeadingTwoWhite from "@/atoms/atoms-shiva/headings/two/xl/white/two-xl"
-import PictureCover from "@/atoms/picture/internal/picture-cover"
+import ButtonCyan from "@/atoms/atoms-shiva/button/cyan/large/jsx/index"
+import ButtonDark from "@/atoms/atoms-shiva/button/dark/jsx/index"
+import ButtonOrange from "@/atoms/atoms-shiva/button/orange/jsx/index"
+import HeadingOneWhite from "@/atoms/atoms-shiva/headings/one/six-xl/white/jsx/index"
+import HeadingTwoWhite from "@/atoms/atoms-shiva/headings/two/xl/white/jsx/index"
+import Link from "@/atoms/links/jsx"
+import PictureCover from "@/atoms/picture/internal/jsx/index"
 
 // HARDCODED DATA
 const gradients = [
@@ -18,54 +22,54 @@ const gradients = [
 ]
 const slides = [
     {
-        title: "THE MEDIA CAPTAIN",
-        heading: "Columbus Digital Marketing & Web Design Firm",
-        subheading: "Voted a Top 1% Agency by Clutch and UpCity. Serving Clients in Ohio and Nationwide.",
         buttonText: "CONTACT US",
         buttonType: "cyan",
-        imageSrc: "/images/hero/about-tmc-slide-3.jpg",
+        heading: "Columbus Digital Marketing & Web Design Firm",
         imageAlt: "Image for Slide 5",
-        orangeWords: ["Columbus"]
+        imageSrc: "/images/hero/about-tmc-slide-3.jpg",
+        orangeWords: ["Columbus"],
+        subheading: "Voted a Top 1% Agency by Clutch and UpCity. Serving Clients in Ohio and Nationwide.",
+        title: "THE MEDIA CAPTAIN",
     },
     {
-        title: "OUR COLUMBUS MARKETING FIRM",
-        heading: "Doubled Grandview Dental's Organic Visibility on Google.",
         buttonText: "WORK FOR US",
         buttonType: "cyan",
-        imageSrc: "/images/hero/tmc-slider-img-1.png",
+        heading: "Doubled Grandview Dental's Organic Visibility on Google.",
         imageAlt: "Image for Slide 2",
-        orangeWords: ["Doubled"]
+        imageSrc: "/images/hero/tmc-slider-img-1.png",
+        orangeWords: ["Doubled"],
+        title: "OUR COLUMBUS MARKETING FIRM",
     },
     {
-        title: "OUR COLUMBUS DIGITAL AGENCY",
-        heading: "Helped Schmidt's Increase Social Media Engagement by 225%",
         buttonText: "WORK FOR US",
         buttonType: "dark",
-        imageSrc: "/images/hero/tmc-slider-img-2.png",
+        heading: "Helped Schmidt's Increase Social Media Engagement by 225%",
         imageAlt: "Image for Slide 3",
-        orangeWords: ["by 225%"]
+        imageSrc: "/images/hero/tmc-slider-img-2.png",
+        orangeWords: ["by 225%"],
+        title: "OUR COLUMBUS DIGITAL AGENCY",
     },
     {
-        title: "THE MEDIA CAPTAIN",
-        heading: "Launched Hundreds of Websites for all Business Types.",
         buttonText: "VIEW PORTFOLIO",
-        buttonType: "orange",
         buttonText2: "WORK FOR US",
+        buttonType: "orange",
         buttonType2: "cyan",
-        imageSrc: "/images/hero/tmc-slider-img-3.png",
+        heading: "Launched Hundreds of Websites for all Business Types.",
         imageAlt: "Image for Slide 4",
-        orangeWords: ["Hundreds"]
+        imageSrc: "/images/hero/tmc-slider-img-3.png",
+        orangeWords: ["Hundreds"],
+        title: "THE MEDIA CAPTAIN",
     },
     {
-        title: "OUR COLUMBUS MARKETING COMPANY",
-        heading: "Helped DermWarehouse Amass Over 400,000 Customers",
         buttonText: "VIEW SUCCESS STORY",
-        buttonType: "orange",
         buttonText2: "WORK WITH US",
+        buttonType: "orange",
         buttonType2: "dark",
-        imageSrc: "/images/hero/tmc-slider-img-4.png",
+        heading: "Helped DermWarehouse Amass Over 400,000 Customers",
         imageAlt: "Image for Slide 5",
-        orangeWords: ["400,000"]
+        imageSrc: "/images/hero/tmc-slider-img-4.png",
+        orangeWords: ["400,000"],
+        title: "OUR COLUMBUS MARKETING COMPANY",
     }
 ]
 
@@ -96,15 +100,18 @@ const HeroSection = () => {
     const formatHeading = (heading, orangeWords) => {
         let formattedHeading = heading
         orangeWords.forEach(word => {
+
             const regex = new RegExp(`(${word})`, 'gi')
             formattedHeading = formattedHeading.replace(regex, '<span class="text-orange-500">$1</span>')
+
         })
         return { __html: formattedHeading }
     }
 
     return (
+
         <div
-            className="relative h-dvh md:h-full w-full md:aspect-[16/7] transition-all duration-1000"
+            className="relative h-full w-full md:aspect-[16/7] transition-all duration-1000"
             style={{ background: currentGradient, transition: "background 4s ease" }}
         >
             {
@@ -115,25 +122,29 @@ const HeroSection = () => {
 
                         <div className="absolute inset-0 grid md:grid-cols-2 gap-6" key={index}>
                             <div className="flex items-center md:justify-center pl-4 lg:pl-24">
-                                <div className="grid gap-4">
+                                <div className="grid gap-6">
                                     <HeadingTwoWhite>{ slide.title }</HeadingTwoWhite>
                                     <HeadingOneWhite>
                                         <span dangerouslySetInnerHTML={formatHeading(slide.heading, slide.orangeWords)} />
                                     </HeadingOneWhite>
                                     {slide.subheading && <HeadingTwoWhite>{ slide.subheading }</HeadingTwoWhite>}
                                     <div className="flex gap-4">
-                                        <a href="/">
-                                            {slide.buttonType === "cyan" && <ButtonCyan button_text={ slide.buttonText } />}
-                                            {slide.buttonType === "dark" && <ButtonDark button_text={ slide.buttonText } />}
-                                            {slide.buttonType === "orange" && <ButtonOrange button_text={ slide.buttonText } />}
-                                        </a>
-                                        {slide.buttonText2 && (
-                                            <a href="/">
-                                                {slide.buttonType2 === "cyan" && <ButtonCyan button_text={ slide.buttonText2 } />}
-                                                {slide.buttonType2 === "dark" && <ButtonDark button_text={ slide.buttonText2 } />}
-                                                {slide.buttonType2 === "orange" && <ButtonOrange button_text={ slide.buttonText2 } />}
-                                            </a>
-                                        )}
+                                        <Link href="/">
+                                            {slide.buttonType === "cyan" && <ButtonCyan children={ slide.buttonText } />}
+                                            {slide.buttonType === "dark" && <ButtonDark children={ slide.buttonText } />}
+                                            {slide.buttonType === "orange" && <ButtonOrange children={ slide.buttonText } />}
+                                        </Link>
+                                        {
+                                            slide.buttonText2 && (
+
+                                                <Link href="/">
+                                                    {slide.buttonType2 === "cyan" && <ButtonCyan children={ slide.buttonText2 } />}
+                                                    {slide.buttonType2 === "dark" && <ButtonDark children={ slide.buttonText2 } />}
+                                                    {slide.buttonType2 === "orange" && <ButtonOrange children={ slide.buttonText2 } />}
+                                                </Link>
+
+                                            )
+                                        }
                                     </div>
                                 </div>
                             </div>
@@ -152,18 +163,20 @@ const HeroSection = () => {
             }
 
             <button
-                className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
+                className="flex items-center justify-center absolute left-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
                 onClick={ handlePrevSlide }
             >
-                ❮
+                <IoIosArrowBack/>
             </button>
             <button
-                className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
+                className="flex items-center justify-center absolute right-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
                 onClick={ handleNextSlide }
             >
-                ❯
+                <IoIosArrowForward/>
             </button>
         </div>
+
     )
+
 }
 export default HeroSection
