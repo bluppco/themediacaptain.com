8fd8546177eed30d68c4cc321361c58f8d0bdc2e
Shiva pandey <shivapandey9898@gmail.com>
2024-07-22 12:43:53 +0530

    refactor: used props.children for atoms

--- src/atoms/atoms-shiva/button/dark/jsx/index.jsx
+++ src/atoms/atoms-shiva/button/dark/jsx/index.jsx
@@ -1,9 +1,9 @@
-const ButtonDark = ({ children }) => {
+const ButtonDark = ( props ) => {
 
     return (
 
-        <button className="lg:px-10 lg:py-4 px-2 py-2 bg-slate-900 hover:bg-orange-500 duration-300 lg:text-xl font-geomanist_book text-white">
-            { children }
+        <button className="bg-slate-900 hover:bg-orange-500 duration-300 lg:text-xl text-white font-geomanist_book lg:px-10 lg:py-4 px-2 py-2">
+            { props.children }
         </button>
         
     )
