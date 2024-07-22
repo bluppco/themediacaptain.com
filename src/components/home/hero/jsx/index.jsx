a21461e9bbee1406f00e1d405ab56eaf6581ed9d
Shiva pandey <shivapandey9898@gmail.com>
2024-07-22 12:43:36 +0530

    refactor: renamed the hero jsx path

--- src/components/home/hero/jsx/index.jsx
+++ src/components/home/hero/jsx/index.jsx
@@ -1,12 +1,14 @@
+// IMPORT REACT HOOKS
 import { useState, useEffect } from "react"
 
 // IMPORT REACT ICONS
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 
-// IMPORT REACT ATOMS
+// IMPORT JSX ATOMS
 import ButtonCyan from "@/atoms/atoms-shiva/button/cyan/large/jsx/index"
 import ButtonDark from "@/atoms/atoms-shiva/button/dark/jsx/index"
 import ButtonOrange from "@/atoms/atoms-shiva/button/orange/jsx/index"
+import ButtonRoundedFull from "@/atoms/atoms-shiva/button/rounded-full/jsx"
 import HeadingOneWhite from "@/atoms/atoms-shiva/headings/one/six-xl/white/jsx/index"
 import HeadingTwoWhite from "@/atoms/atoms-shiva/headings/two/xl/white/jsx/index"
 import Link from "@/atoms/links/jsx"
@@ -14,14 +16,18 @@ import PictureCover from "@/atoms/picture/internal/jsx/index"
 
 // HARDCODED DATA
 const gradients = [
+
     "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(87,116,133,1) 49%, rgba(138,189,208,1) 100%)",
     "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(87,116,133,1) 49%, rgba(138,189,208,1) 100%)",
     "linear-gradient(90deg, rgba(138,189,208,1) 0%, rgba(87,116,133,1) 49%, rgba(2,0,36,1) 100%)",
     "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(87,116,133,1) 49%, rgba(138,189,208,1) 100%)",
     "linear-gradient(90deg, rgba(138,189,208,1) 0%, rgba(87,116,133,1) 49%, rgba(2,0,36,1) 100%)",
+
 ]
 const slides = [
+
     {
+
         buttonText: "CONTACT US",
         buttonType: "cyan",
         heading: "Columbus Digital Marketing & Web Design Firm",
@@ -30,8 +36,10 @@ const slides = [
         orangeWords: ["Columbus"],
         subheading: "Voted a Top 1% Agency by Clutch and UpCity. Serving Clients in Ohio and Nationwide.",
         title: "THE MEDIA CAPTAIN",
+
     },
     {
+
         buttonText: "WORK FOR US",
         buttonType: "cyan",
         heading: "Doubled Grandview Dental's Organic Visibility on Google.",
@@ -39,8 +47,10 @@ const slides = [
         imageSrc: "/images/hero/tmc-slider-img-1.png",
         orangeWords: ["Doubled"],
         title: "OUR COLUMBUS MARKETING FIRM",
+
     },
     {
+
         buttonText: "WORK FOR US",
         buttonType: "dark",
         heading: "Helped Schmidt's Increase Social Media Engagement by 225%",
@@ -48,8 +58,10 @@ const slides = [
         imageSrc: "/images/hero/tmc-slider-img-2.png",
         orangeWords: ["by 225%"],
         title: "OUR COLUMBUS DIGITAL AGENCY",
+
     },
     {
+
         buttonText: "VIEW PORTFOLIO",
         buttonText2: "WORK FOR US",
         buttonType: "orange",
@@ -59,8 +71,10 @@ const slides = [
         imageSrc: "/images/hero/tmc-slider-img-3.png",
         orangeWords: ["Hundreds"],
         title: "THE MEDIA CAPTAIN",
+
     },
     {
+        
         buttonText: "VIEW SUCCESS STORY",
         buttonText2: "WORK WITH US",
         buttonType: "orange",
@@ -70,42 +84,64 @@ const slides = [
         imageSrc: "/images/hero/tmc-slider-img-4.png",
         orangeWords: ["400,000"],
         title: "OUR COLUMBUS MARKETING COMPANY",
+
     }
+
 ]
 
 const HeroSection = () => {
+
+    // TRACK SLIDES
     const [currentSlide, setCurrentSlide] = useState(0)
     const [currentGradient, setCurrentGradient] = useState(gradients[0])
 
+    // AUTO CHANGE CURRENT SLIDES WITH TIME DELAY
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentSlide(prev => (prev + 1) % slides.length)
-        }, 20000)
 
-        return () => clearInterval(interval)
+        const interval = setInterval(() => {
+
+            setCurrentSlide ( prev => ( prev + 1 ) % slides.length )
+
+        }, 10000)
+
+        return () => clearInterval( interval  )
+
     }, [])
 
+    // CHANGE THE SLIDES GRADIENT
     useEffect(() => {
-        setCurrentGradient(gradients[currentSlide])
+
+        setCurrentGradient( gradients[currentSlide] )
+
     }, [currentSlide])
 
+    // GO TO NEXT SLIDE
     const handleNextSlide = () => {
-        setCurrentSlide(prev => (prev + 1) % slides.length)
+
+        setCurrentSlide( prev => ( prev + 1 ) % slides.length )
+
     }
 
+    // GO TO PREVIOUS SLIDE
     const handlePrevSlide = () => {
-        setCurrentSlide(prev => (prev - 1 + slides.length) % slides.length)
+
+        setCurrentSlide( prev => ( prev - 1 + slides.length ) % slides.length )
+
     }
 
-    const formatHeading = (heading, orangeWords) => {
-        let formattedHeading = heading
-        orangeWords.forEach(word => {
+    // FORMAT DATA FOR HEADING TEXT TO ADD COLORED TEXT
+    const formatHeading = ( heading, orangeWords ) => {
 
-            const regex = new RegExp(`(${word})`, 'gi')
+        let formattedHeading = heading
+        orangeWords.forEach( word => {
+
+            const regex = new RegExp(`(${ word })`, 'gi')
             formattedHeading = formattedHeading.replace(regex, '<span class="text-orange-500">$1</span>')
 
         })
+
         return { __html: formattedHeading }
+
     }
 
     return (
@@ -116,39 +152,45 @@ const HeroSection = () => {
         >
             {
             
-                slides.map((slide, index) => (
+                slides.map( ( slide, index ) => (
 
                     currentSlide === index && (
 
-                        <div className="absolute inset-0 grid md:grid-cols-2 gap-6" key={index}>
+                        <div className="absolute inset-0 mt-6 md:mt-0 grid md:grid-cols-2 gap-6" key={ index }>
                             <div className="flex items-center md:justify-center pl-4 lg:pl-24">
                                 <div className="grid gap-6">
                                     <HeadingTwoWhite>{ slide.title }</HeadingTwoWhite>
                                     <HeadingOneWhite>
-                                        <span dangerouslySetInnerHTML={formatHeading(slide.heading, slide.orangeWords)} />
+                                        <span dangerouslySetInnerHTML={ formatHeading( slide.heading, slide.orangeWords ) } />
                                     </HeadingOneWhite>
-                                    {slide.subheading && <HeadingTwoWhite>{ slide.subheading }</HeadingTwoWhite>}
+                                    {
+
+                                        slide.subheading && <HeadingTwoWhite>{ slide.subheading }</HeadingTwoWhite>
+
+                                    }
                                     <div className="flex gap-4">
                                         <Link href="/">
-                                            {slide.buttonType === "cyan" && <ButtonCyan children={ slide.buttonText } />}
-                                            {slide.buttonType === "dark" && <ButtonDark children={ slide.buttonText } />}
-                                            {slide.buttonType === "orange" && <ButtonOrange children={ slide.buttonText } />}
+                                            { slide.buttonType === "cyan" && <ButtonCyan children={ slide.buttonText } /> }
+                                            { slide.buttonType === "dark" && <ButtonDark children={ slide.buttonText } /> }
+                                            { slide.buttonType === "orange" && <ButtonOrange children={ slide.buttonText } /> }
                                         </Link>
                                         {
+
                                             slide.buttonText2 && (
 
                                                 <Link href="/">
-                                                    {slide.buttonType2 === "cyan" && <ButtonCyan children={ slide.buttonText2 } />}
-                                                    {slide.buttonType2 === "dark" && <ButtonDark children={ slide.buttonText2 } />}
-                                                    {slide.buttonType2 === "orange" && <ButtonOrange children={ slide.buttonText2 } />}
+                                                    { slide.buttonType2 === "cyan" && <ButtonCyan children={ slide.buttonText2 } /> }
+                                                    { slide.buttonType2 === "dark" && <ButtonDark children={ slide.buttonText2 } /> }
+                                                    { slide.buttonType2 === "orange" && <ButtonOrange children={ slide.buttonText2 } /> }
                                                 </Link>
 
                                             )
+
                                         }
                                     </div>
                                 </div>
                             </div>
-                            <div className="overflow-hidden h-full w-full aspect-square">
+                            <div className="overflow-hidden h-full w-full">
                                 <PictureCover
                                     alternative_text={ slide.imageAlt }
                                     source={ slide.imageSrc }
@@ -161,19 +203,12 @@ const HeroSection = () => {
                 ))
             
             }
-
-            <button
-                className="flex items-center justify-center absolute left-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
-                onClick={ handlePrevSlide }
-            >
-                <IoIosArrowBack/>
-            </button>
-            <button
-                className="flex items-center justify-center absolute right-4 top-1/2 transform -translate-y-1/2 bg-black size-14 text-white text-2xl bg-opacity-50 p-2 rounded-full"
-                onClick={ handleNextSlide }
-            >
-                <IoIosArrowForward/>
-            </button>
+            <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
+                <ButtonRoundedFull click_event={ handlePrevSlide }><IoIosArrowBack/></ButtonRoundedFull>
+            </div>
+            <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
+                <ButtonRoundedFull click_event={ handleNextSlide }><IoIosArrowForward/></ButtonRoundedFull>
+            </div>
         </div>
 
     )
